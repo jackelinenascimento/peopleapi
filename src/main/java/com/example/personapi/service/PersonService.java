7ce997e9ecd5f9b80a006fee7cee514d15380476
Jackeline Nascimento <jackeline_nascimento@hotmail.com>
2021-09-30 13:19:16 -0300

    included GET method

--- src/main/java/com/example/personapi/service/PersonService.java
+++ src/main/java/com/example/personapi/service/PersonService.java
@@ -9,6 +9,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
@@ -31,5 +34,14 @@ public class PersonService {
         return MessageResponseDTO.builder()
                 .message(s + id2)
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+
+        List<Person> allPeople = personRepository.findAll();
+
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
