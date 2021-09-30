00b397a1159f85070892a6eebf3a89492b55c8e9
Jackeline Nascimento <jackeline_nascimento@hotmail.com>
2021-09-30 12:28:20 -0300

    added PersonMapper

--- src/main/java/com/example/personapi/service/PersonService.java
+++ src/main/java/com/example/personapi/service/PersonService.java
@@ -1,24 +1,35 @@
 package com.example.personapi.service;
 
-import com.example.personapi.dto.MessageResponseDTO;
+import com.example.personapi.dto.request.PersonDTO;
+import com.example.personapi.dto.response.MessageResponseDTO;
 import com.example.personapi.entity.Person;
+import com.example.personapi.dto.mapper.PersonMapper;
 import com.example.personapi.repository.PersonRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
 
-    private PersonRepository personRepository;
+    private final PersonRepository personRepository;
 
-    public PersonService(PersonRepository personRepository){
-        this.personRepository = personRepository;
+    private final PersonMapper personMapper;
+
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
+
+        Person person = personMapper.toModel(personDTO);
+        Person savedPerson = personRepository.save(person);
+
+        MessageResponseDTO messageResponse = createMessageResponse("Person successfully created with ID ", savedPerson.getId());
+
+        return messageResponse;
     }
 
-    public MessageResponseDTO createPerson(Person person) {
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID: " + savedPerson.getId())
+    private MessageResponseDTO createMessageResponse(String s, Long id2) {
+        return MessageResponseDTO.builder()
+                .message(s + id2)
                 .build();
     }
 }
