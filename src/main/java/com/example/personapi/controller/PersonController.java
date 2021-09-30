dbfbd8096240715a26b6a6eb0d6c2c8b522d067a
Jackeline Nascimento <jackeline_nascimento@hotmail.com>
2021-09-29 22:45:05 -0300

    created PersonController and MessageResponseDTO

--- src/main/java/com/example/personapi/controller/PersonController.java
+++ src/main/java/com/example/personapi/controller/PersonController.java
@@ -1,15 +1,33 @@
 package com.example.personapi.controller;
 
+import com.example.personapi.dto.MessageResponseDTO;
+import com.example.personapi.entity.Person;
+import com.example.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getPeople(){
-        return "API Rest";
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
     }
 }
